7e17713e8b8e26f55509511afe7428973ae6734d
Matthew Donkor <thematthewdonkor@gmail.com>
2025-09-01 02:57:08 +0000

    suspense use on page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { Sidebar } from "@/components/sidebar";
 import { Header } from "@/components/header";
 import { HeroSection } from "@/components/hero-section";
 import { MovieCard } from "@/components/movie-card";
-import { Loading } from "@/components/loading";
 
 export default function Home() {
   return (
@@ -12,13 +11,15 @@ export default function Home() {
       <div className="flex">
         <Sidebar />
         <div className="flex-1 min-w-0">
-          <Suspense fallback={<Loading />}>
+          <Suspense>
             <Header />
           </Suspense>
           <main className="p-4 md:p-6">
             <HeroSection />
             <div className="mt-6 md:mt-8 pb-4">
-              <MovieCard />
+              <Suspense>
+                <MovieCard />
+              </Suspense>
             </div>
           </main>
         </div>
