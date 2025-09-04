ddbad24d8621fef1e84330c580816fd2f1bcdad3
Matthew Donkor <thematthewdonkor@gmail.com>
2025-09-04 01:44:56 +0000

    Token pass directly

--- actions/getMovieDetails.ts
+++ actions/getMovieDetails.ts
@@ -9,7 +9,7 @@ export const getMovieDetails = async (id: number) => {
       {
         headers: {
           Accept: "application/json",
-          Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTAxODk1MTVhY2QxYWRhNTlmYTFkZjAxM2E4ZmVhNCIsIm5iZiI6MTc1NDI2NTkzOS42NjYsInN1YiI6IjY4OGZmOTUzZTg1ZTkxNDM2ZWYzZTRkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MAZnPMwBzP9qq3tPMzgrvaf6f7NowXXkNZ15kHlRqQo`,
         },
       }
     );
